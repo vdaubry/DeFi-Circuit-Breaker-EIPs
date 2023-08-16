6e1a94d7b19b49b0baba876ae8e3a951fff7ef30
keinberger <philipp.keinberger@gmail.com>
2023-08-16 16:51:00 +0200

    feat: add CircuitBreaker interface natspec

--- assets/eip-7265/src/interfaces/IERC7265CircuitBreaker.sol
+++ assets/eip-7265/src/interfaces/IERC7265CircuitBreaker.sol
@@ -26,9 +26,17 @@ interface IERC7265CircuitBreaker {
 
     /**
      * @notice Function for increasing the current security parameter
+     * @param identifier is the identifier of the security parameter
+     * Every security parameter has a unique bytes32 identifier. This allows for configuring multiple security parameters, for
+     * multiple metrics within a single protocol (e.g. a protocol with multiple assets, or a protocol with multiple markets)
+     * @param amount is the amount by which the security parameter is increased
+     * @param settlementTarget is the target address for a potential settlement at the settlement module
+     * This address gets called by the settlement module in case of the CircuitBreaker being triggered
+     * @param settlementValue is the value for a potential settlement at the settlement module
+     * This value gets sent to the settlement target by the settlement module in case of the CircuitBreaker being triggered
+     * @param settlementPayload is the payload for a potential settlement at the settlement module
+     * This payload gets sent to the settlement target by the settlement module in case of the CircuitBreaker being triggered
      * @dev This function MAY only be called by the owner of the security parameter
-     * // bytes32 identifier
-     * // revertOnRateLimit
      * The function MUST emit the {ParameterSet} event
      */
     function increaseParameter(
@@ -41,9 +49,17 @@ interface IERC7265CircuitBreaker {
 
     /**
      * @notice Function for decreasing the current security parameter
+     * @param identifier is the identifier of the security parameter
+     * Every security parameter has a unique bytes32 identifier. This allows for configuring multiple security parameters, for
+     * multiple metrics within a single protocol (e.g. a protocol with multiple assets, or a protocol with multiple markets)
+     * @param amount is the amount by which the security parameter is increased
+     * @param settlementTarget is the target address for a potential settlement at the settlement module
+     * This address gets called by the settlement module in case of the CircuitBreaker being triggered
+     * @param settlementValue is the value for a potential settlement at the settlement module
+     * This value gets sent to the settlement target by the settlement module in case of the CircuitBreaker being triggered
+     * @param settlementPayload is the payload for a potential settlement at the settlement module
+     * This payload gets sent to the settlement target by the settlement module in case of the CircuitBreaker being triggered
      * @dev This function MAY only be called by the owner of the security parameter
-     * // bytes32 identifier
-     * // revertOnRateLimit
      * The function MUST emit the {ParameterSet} event
      */
     function decreaseParameter(
@@ -55,38 +71,75 @@ interface IERC7265CircuitBreaker {
     ) external returns (bool);
 
     /**
+     * @notice Function for adding a security parameter
+     * @param identifier is the identifier of the security parameter
+     * Every security parameter has a unique bytes32 identifier. This allows for configuring multiple security parameters, for
+     * multiple metrics within a single protocol (e.g. a protocol with multiple assets, or a protocol with multiple markets)
+     * @param minValBps is the minimum amount a security parameter can reach in percent before the Circuit Breaker is triggered
+     * @param limitBeginThreshold is the minimal amount of a security parameter that MUST be reached before the Circuit Breaker checks for a breach
+     * This limits potential false positives triggered either by minor assets with low liquidity or by low liquidity during early stages of protocol launch.
+     * Below this amount, withdrawals of this asset MUST NOT trigger a rate limit.
+     * However, if a rate limit is triggered, assets below the minimum trigger amount to limit MUST still be locked.
+     * @param settlementModule is the address of the settlement module
      * @dev MAY be called by admin to configure a security parameter
      */
     function addSecurityParameter(
         bytes32 identifier,
-        uint256 minLiqRetainedBps,
+        uint256 minValBps,
         uint256 limitBeginThreshold,
         address settlementModule
     ) external;
 
     /**
+     * @notice Function for updating the configuraiton for a security parameter
+     * @param identifier is the identifier of the security parameter
+     * Every security parameter has a unique bytes32 identifier. This allows for configuring multiple security parameters, for
+     * multiple metrics within a single protocol (e.g. a protocol with multiple assets, or a protocol with multiple markets)
+     * @param minValBps is the minimum amount a security parameter can reach in percent before the Circuit Breaker is triggered
+     * @param limitBeginThreshold is the minimal amount of a security parameter that MUST be reached before the Circuit Breaker checks for a breach
+     * This limits potential false positives triggered either by minor assets with low liquidity or by low liquidity during early stages of protocol launch.
+     * Below this amount, withdrawals of this asset MUST NOT trigger a rate limit.
+     * However, if a rate limit is triggered, assets below the minimum trigger amount to limit MUST still be locked.
+        * @param settlementModule is the address of the settlement module
      * @dev MAY be called by admin to update configuration of a security parameter
      */
     function updateSecurityParameter(
         bytes32 identifier,
-        uint256 minLiqRetainedBps,
+        uint256 minValBps,
         uint256 limitBeginThreshold,
         address settlementModule
     ) external;
 
+    /**
+     * @notice Function for returning if a security parameter is breached
+     * @param identifier is the identifier of the security parameter
+     * @dev MUST return true if the security parameter is breached
+     */
     function isRateLimited(bytes32 identifier) external view returns (bool);
 
     /**
-     * @dev MAY be called by admin to add protected contracts
-     */
+     * @notice Add new protected contracts
+     * @param _protectedContracts an array of addresses of protected contracts to add
+     * @dev MUST be used to add protected contracts. Protected contracts MUST be part of your protocol. 
+     * Protected contracts have the authority to trigger rate limits and withdraw assets. 
+     * MUST revert if caller is not the current admin.
+     * MUST store protected contracts in the stored state of the circuit breaker implementation.
+     */ 
     function addProtectedContracts(address[] calldata _protectedContracts) external;
 
     /**
-     * @dev MAY be called by admin to add protected contracts
+     * @notice Remove protected contracts
+     * @param _protectedContracts an array of addresses of protected contracts to remove
+     * @dev MAY be used to remove protected contracts. Protected contracts MUST be part of your protocol.
+     * Protected contracts have the authority to trigger rate limits and withdraw assets.
+     * MUST revert if caller is not the current admin.
+     * MUST remove protected contracts from stored state in the circuit breaker implementation.
      */
     function removeProtectedContracts(address[] calldata _protectedContracts) external;
 
     /// @notice Function for pausing / unpausing the Circuit Breaker
+    /// @param newOperationalStatus is the new operational status of the Circuit Breaker (true = operational, false = not operational)
+    /// @dev MUST revert if caller is not the current admin.
     /// @dev MAY be called by admin to pause / unpause the Circuit Breaker
     /// While the protocol is not operational: inflows, outflows, and claiming locked funds MUST revert
     function setCircuitBreakerOperationalStatus(bool newOperationalStatus) external;
