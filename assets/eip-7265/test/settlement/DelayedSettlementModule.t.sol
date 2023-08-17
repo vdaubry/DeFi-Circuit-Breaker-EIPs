7d186b739d6bcff8513bd99fbb30f6c0f3b3b6d0
Vincent Daubry <vdaubry@gmail.com>
2023-08-17 13:56:18 +0200

    add spec for DSM schedule function

---
+++ assets/eip-7265/test/settlement/DelayedSettlementModule.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "forge-std/console.sol";
+import {Test} from "forge-std/Test.sol";
+import {MockToken} from "../mocks/MockToken.sol";
+import {DelayedSettlementModule} from "../../src/settlement/DelayedSettlementModule.sol";
+
+contract DelayedSettlementModuleTest is Test {
+    MockToken internal token;
+    DelayedSettlementModule internal delayedSettlementModule;
+
+    address internal alice = vm.addr(0x1);
+    address internal bob = vm.addr(0x2);
+    address internal admin = vm.addr(0x3);
+
+    function setUp() public {
+        token = new MockToken("USDC", "USDC");
+        address[] memory admins = new address[](1);
+        admins[0] = admin;
+
+        delayedSettlementModule = new DelayedSettlementModule(
+            1 seconds,
+            admins, // proposer = admin
+            admins, // executor = admin
+            admin
+        );
+    }
+
+    function test_prevent() public {
+        bytes memory innerPayload = abi.encodeWithSignature(
+            "transfer(address,uint256)",
+            bob,
+            1_000e18
+        );
+
+        vm.prank(admin);
+        bytes32 effectID = delayedSettlementModule.prevent(
+            address(token),
+            0,
+            innerPayload
+        );
+        assertEq(
+            effectID,
+            keccak256(abi.encode(address(token), 0, innerPayload)),
+            "Effect ID mismatch."
+        );
+    }
+}
